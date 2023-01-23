21855c42def7f1846509e80c030cd43bf6c1ec9d
alyssabenipayo <alymaebenipayo@gmail.com>
2023-01-22 22:33:02 -0800

    Save progress - Class in Session.

--- main.js
+++ main.js
@@ -1,44 +1,31 @@
 'use strict';
 
-// Traditional JavaScript
-// function nameString() {
-//     return `${username.name} ({$username.email})`;
-// }
-
-// Modern JavaScript: Arrow Functions (or Arrow Notation)
-// const nameString = (name, isTrue) => `${name} ({$username.email})`;
-
-// document.getElementById('output').innerHTML = nameString('Alyssa', true);
-
-let users = [
-    {
-        name: "Alyssa",
-        email: "alyssa@example.com"
-    },
-    {
-        name: "Ashley",
-        email: "ashley@example.com"
-    }
-];
-
-let names = [];
-
-// // Traditional JavaScript
-// users.forEach(function(user){
-//     //do something with that user object
-//     names.push(user.name)
-// });
-
-users.forEach(user => names.push(user.name));
-
-document.getElementById('output').innerHTML = names.join(', ');
-
-document.getElementById('btn').addEventListener('click', (e) => {
-
-    const getDetails = () => {
-        return `The button id is ${e.currentTarget.getAttribute('id')}`;
+class Item {
+    constructor(name, category) {
+        this.name = name;
+        this.category = category;
     }
 
-    document.getElementById('output').innerHTML = getDetails();
+    getDetails() {
+        return `${this.name} - ${this.category}`;
+    }
+}
 
-});
+class PurchasedItem extends Item {
+    constructor(name, category, price) {
+        // Helper method that calls the constructor for the class (Item) by using the context of PurchasedItems.
+        super(name, category);
+        this.price = price;
+    }
+
+    getDetailsWithPrice() {
+        return `${this.name} - ${this.category} - $${this.price}`;
+    }
+}
+
+let item = new Item('Coffee', 'Food');
+item.category = 'Drinks';
+
+let purchasedItem = new PurchasedItem('Sugar', 'Food', '2.49');
+
+document.getElementById('output').innerHTML = purchasedItem.getDetailsWithPrice();
