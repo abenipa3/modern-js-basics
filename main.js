c7b2c42860b56e0beb45154f8d96e19ecf8950ff
alyssabenipayo <alymaebenipayo@gmail.com>
2023-01-31 20:37:18 -0800

    Getting started with Maps: Maps Part 1.

--- main.js
+++ main.js
@@ -1,41 +1,67 @@
-// Async and Await
+// Maps and Sets are Special Objects
 
 'use strict';
 
-async function init() {
-    // start
-    const start = Date.now();
-    document.getElementById('output').innerHTML = `0: init()`;
+//  Map is an object that holds a key value pair.
 
-    // both will start at the same time
-    const userPromise = getUserData();
-    const welcomeStringPromise = getWelcomeString();
+// const fruit = new Map();
 
-    // wait two seconds
-    const user = await userPromise;
-    document.getElementById('output').innerHTML += `<br>${Date.now() - start}: ${user.name}`;
-    // wait two seconds before it shows up. will not show up until getUserData completed.
-    const welcomeString = await welcomeStringPromise;
-    document.getElementById('output').innerHTML += `<br>${Date.now() - start}: ${welcomeString}`;
-}
+// fruit.set(1, 'Banana');
+// fruit.set(2, 'Apple');
+// fruit.set(3, 'Strawberry');
 
-function getUserData() {
-    return new Promise((resolve, reject) => {
-        let user = {
-            name: 'Andrew',
-            email: 'andrew@example.com'
-        };
-        setTimeout(() => {
-            resolve(user);
-        }, 2000);
-    })
-}
+// document.getElementById('output').innerHTML = fruit.get(2);
 
-function getWelcomeString() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Welcome to my asyncronous program!');
-        }, 2000);
-    })
-}
-init();
+// fruit.delete(2);
+
+
+// fruit.forEach(item => {
+//     document.getElementById('output').innerHTML += '<br>' + item;
+// });
+
+// The key and the value can be anything.
+
+// fruit.set(4, {
+//     type: 'Orange',
+//     color: 'orange',
+//     flavor: 'orangey'
+// });
+
+// document.getElementById('output').innerHTML = fruit.get(4).flavor;
+
+const users = new Map();
+
+users.set({
+    name: 'Alyssa',
+    email: 'alyssa@example.com'
+}, function(user){
+    document.getElementById('output').innerHTML += `${user.name} (${user.email}) <br>`;
+});
+
+users.set({
+    name: 'Ashley',
+    email: 'ashley@example.com'
+}, function(user){
+    document.getElementById('output').innerHTML += `(${user.email}) ${user.name} <br>`;
+});
+
+// The value is the function. The key are what we set in the map.
+users.forEach((value, key) => {
+    value(key);
+});
+
+// A has method where we pass in an idea and 
+// it will turn into a boolean (true or false)
+// if the item exists in the map.
+let userAshleyExists = users.has({
+    name: 'Ashley',
+    email: 'ashley@example.com'
+});
+
+// Should return true because the item DOES exist in
+// the map that has the following as the key:
+console.log('userAshleyExists: ', userAshleyExists);
+
+// Spoiler alert: console log reveals false because
+// although an item shows in the users map, it's
+//  not the same object as if they are two different keys.""
